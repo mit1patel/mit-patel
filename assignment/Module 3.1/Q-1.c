52ce6d51c399487bd912bcc25dad8be47c52511f
mit1patel <mitkanetiya@gmail.com>
2023-09-25 18:09:44 +0530

    Done

---
+++ assignment/Module 3.1/Q-1.c
@@ -0,0 +1,13 @@
+/* Display This Information using printf
+1. Your Name
+2. Your Birth date
+3. Your Age
+4. Your Address */
+#include <stdio.h>
+main()
+{
+    printf("NAME:Mit kanetiya\n");
+    printf("BIRTH DATE:06-11-2004\n");
+    printf("AGE:18\n");
+    printf("ADDRESS:Mathav park,botad\n");
+}
